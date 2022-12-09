5bec7664407b034bc4a57cc2826a4e376edfe5bf
JOHN KIMAIYO <87382617+JohnKimaiyo@users.noreply.github.com>
2022-12-09 13:05:27 +0300

    name usestate

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Greet from "./components/Greet";
 import Welcome from "./components/Welcome";
 import Hello from "./components/Hello";
 import Title from "./components/Title";
+import Myname from "./components/Myname";
 function App() {
   return (
     <div className="App">
@@ -13,6 +14,7 @@ function App() {
       <Title name="Frontend developer" />
       <Title name="Mobile developer" />
       <Title name="Solidity developer" />
+      <Myname/>
     </div>
   );
 }
